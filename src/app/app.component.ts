638cfe7a7075d1d8346b98dd3ea1d12dcc96c06c
jsimonetta-vstour <jsimonetta@vstour.com>
2026-01-08 11:34:57 -0300

    Cambios diseño y funcionalidad agregados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'prensa-lvf';
+export class AppComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  verificado: boolean = false;
+  informacion: any = {}
+  intentos: number = 0;
+
+
+  prensaHabilidata = [
+    {
+      Nombre: 'Milagros Simonetta',
+      Dni: '43610607',
+      HabilitadoPor: ['FutFem', 'LVF']
+    }
+  ]
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.verificado = false;
+    this.form = this.fb.group({
+      Dni: ['', [Validators.required, Validators.pattern('^[0-9]{8}$')]]
+    });
+  }
+
+  
+
+  submitForm(){
+    this.prensaHabilidata.forEach(persona => {
+      if (+persona.Dni === this.form.value.Dni){
+        this.verificado = true;
+        this.informacion = persona;
+      }
+    });
+    if (!this.verificado){ 
+      this.intentos += 1;
+    }
+  }
 }
